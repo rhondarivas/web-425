c2e9158ffdc4a3871006d02ea931070e91906db6
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-09-11 18:16:10 -0400

    week 1 updates

---
+++ week-1/ts-examples/person.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * Title: person.class.ts
+ * Author: Rhonda Rivas
+ * Date: September 10 2020
+ * Description: Person class that implements a Person interface
+ */
+
+export interface IPerson {
+  firstName: string;
+  lastName: string;
+}
