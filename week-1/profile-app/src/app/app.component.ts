1e71c2518fbc47978ac3c8f12ca9ef4b74e86bf7
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-09-11 20:13:18 -0400

    week 1 updates

---
+++ week-1/profile-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Title: app.component.ts
+ * Author: Rhonda
+ * Date: September 11 2020
+ * Description: App component
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string = 'Assignment 1.5 - Components';
+}
---
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,23 @@
+
+/**
+ * Title: my-details.component.ts
+ * Author: Rhonda Rivas
+ * Date: September 11 2020
+ * Description: My details component (Details view)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
