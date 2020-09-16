c4e740a787d945f5a1a3841a8c30bd0b0b27612e
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-09-15 21:49:05 -0400

    week 3 updates

---
+++ week-3/secure-profile-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: my-image.component.ts
+ * Author: Rhonda Rivas
+ * Date: September 15 2020
+ * Description: App Component
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Enhanced Profile App';
+}
