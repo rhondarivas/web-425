cff0e3ec922bdef92a889d7061ae9f65cb216cc0
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-09-27 14:13:00 -0400

    week 4 updates

---
+++ week-4/reactive-composer-app/src/app/contact/contact.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: contact.component.ts
+ * Author: Rhonda Rivas
+ * Date: September 13 2020
+ * Description: Contact component (Contact Us page)
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
