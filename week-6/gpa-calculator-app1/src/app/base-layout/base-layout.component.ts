eacd7ca2b8fc46a2b9063fe060979dcba2102568
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-10-16 21:59:02 -0400

    week 6 updates

---
+++ week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
@@ -0,0 +1,25 @@
+/**
+ * Title: base-layout.component.ts
+ * Author: Rhonda Rivas
+ * Date: October 16 2020
+ * Description: About component (About page)
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-layout',
+  templateUrl: './base-layout.component.html',
+  styleUrls: ['./base-layout.component.css']
+})
+export class BaseLayoutComponent implements OnInit {
+
+  assignment: string;
+
+  constructor() {
+    this.assignment = 'Exercise 6.3 - Layouts';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
